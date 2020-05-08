e74ad1ea38e8c23db667e853796e8459eefb19e0
Evan Bernard <epaulbernard@gmail.com>
2020-05-08 03:03:38 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,17 @@
-#require 'pry'
+require 'pry'
 
 def unsafe?(speed)
-  if speed < 40 || speed > 60
-    "false"
-  else
+  if speed < 40
     "true"
+  elseif speed > 60
+    "true"
+  else
+    "false"
   end
 end
 
 
-#binding.pry
+binding.pry
 def not_safe?(speed)
 	
 end
